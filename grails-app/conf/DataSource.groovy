fdb9827ff292a5246fb4c6f3daa7a239e2279088
Renan <renan@itexto.com.br>
2021-03-09 10:56:18 -0300

    war

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -9,7 +9,7 @@ hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     //    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
-    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
     dialect = 'org.hibernate.dialect.MySQL8InnoDBDialect'
     singleSession = true // configure OSIV singleSession mode
     flush.mode = 'manual' // OSIV session flush mode outside of transactional context
@@ -21,36 +21,37 @@ environments {
         dataSource {
             dbCreate = 'update' // one of 'create', 'create-drop', 'update', 'validate', ''
             url = 'jdbc:mysql://localhost:3306/devall'
-    }
-    test {
-        dataSource {
-            dbCreate = 'update'
-            url = 'jdbc:mysql://localhost:3306/devall'
         }
-    }
-    production {
-        dataSource {
-            dbCreate = 'update'
-            url = 'jdbc:mysql://localhost:3306/devall'
-            properties {
-                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-                jmxEnabled = true
-                initialSize = 5
-                maxActive = 50
-                minIdle = 5
-                maxIdle = 25
-                maxWait = 10000
-                maxAge = 10 * 60000
-                timeBetweenEvictionRunsMillis = 5000
-                minEvictableIdleTimeMillis = 60000
-                validationQuery = 'SELECT 1'
-                validationQueryTimeout = 3
-                validationInterval = 15000
-                testOnBorrow = true
-                testWhileIdle = true
-                testOnReturn = false
-                jdbcInterceptors = 'ConnectionState'
-                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+        test {
+            dataSource {
+                dbCreate = 'update'
+                url = 'jdbc:mysql://localhost:3306/devall'
+            }
+        }
+        production {
+            dataSource {
+                dbCreate = 'update'
+                url = 'jdbc:mysql://localhost:3306/devall'
+                properties {
+                    // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                    jmxEnabled = true
+                    initialSize = 5
+                    maxActive = 50
+                    minIdle = 5
+                    maxIdle = 25
+                    maxWait = 10000
+                    maxAge = 10 * 60000
+                    timeBetweenEvictionRunsMillis = 5000
+                    minEvictableIdleTimeMillis = 60000
+                    validationQuery = 'SELECT 1'
+                    validationQueryTimeout = 3
+                    validationInterval = 15000
+                    testOnBorrow = true
+                    testWhileIdle = true
+                    testOnReturn = false
+                    jdbcInterceptors = 'ConnectionState'
+                    //defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+                }
             }
         }
     }
